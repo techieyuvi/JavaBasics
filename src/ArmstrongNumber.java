3f4f9149110353ddc71f17b55a4de9f97ef0a174
techieyuvi <yuvaraj.k1782@gmail.com>
2025-01-05 02:31:11 +0530

    unfinished ArmstrongNum

---
+++ src/ArmstrongNumber.java
@@ -0,0 +1,23 @@
+import java.util.
+
+public static boolean isArmstrongNumber(int number){
+    int originalNum = number;
+    int sum = 0;
+    int numDigits = String.valueOf(number).length();
+
+
+}
+
+
+
+
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        int num;
+        Scanner input = new Scanner(System.in);
+        num = input.nextInt();
+        input.close();
+
+        Boolean isArmstrong = isArmstrongNumber
+    }
+}
