ea4f976f4d4ad64662925a6243cc8e1bfeef24e9
techieyuvi <yuvaraj.k1782@gmail.com>
2025-01-05 02:44:52 +0530

    final Armstrong number

--- src/ArmstrongNumber.java
+++ src/ArmstrongNumber.java
@@ -1,23 +1,34 @@
-import java.util.
-
-public static boolean isArmstrongNumber(int number){
-    int originalNum = number;
-    int sum = 0;
-    int numDigits = String.valueOf(number).length();
-
-
-}
-
+import java.util.Scanner;
 
 
 
 public class ArmstrongNumber {
+
+    public static boolean tofindArmstrong(int number){
+        int originalNum = number;
+        int sum = 0;
+        int numDigits = String.valueOf(number).length();
+
+        while(number != 0){
+            int digit = number % 10;
+            sum += Math.pow(digit,numDigits);
+            number /= 10;
+        }
+        return sum == originalNum;
+    }
+
     public static void main(String[] args) {
         int num;
         Scanner input = new Scanner(System.in);
         num = input.nextInt();
         input.close();
 
-        Boolean isArmstrong = isArmstrongNumber
+        tofindArmstrong(num);
+
+        if(tofindArmstrong(num)){
+            System.out.println("The given number is Armstrong number");
+        }else {
+            System.out.println("The given number is not a Armstrong number");
+        }
     }
 }
