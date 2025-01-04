de1367d5bb8420a292c22c0476995c3be72d8af4
techieyuvi <yuvaraj.k1782@gmail.com>
2025-01-05 00:24:16 +0530

    doc update

--- src/Anagrams.java
+++ src/Anagrams.java
@@ -11,10 +11,8 @@ public class Anagrams {
 
         Scanner sc = new Scanner(System.in);
 
-        // Creating a continuous user interaction using while loop
-        // and also checking the validness of inputs.
-
-        while (true) {
+        while (true) {     // Creating a continuous user interaction using while loop
+            // and also checking the validness of inputs.
 
             System.out.println("Enter the words you want to check:");
             String receivedInput = sc.nextLine();
@@ -28,11 +26,7 @@ public class Anagrams {
 
         }
 
-      /*  if (arr[0].equals(arr[1])) {
-            System.out.println("both the words are same");
-        } else {
-            System.out.println("are not same");
-        } */
+        // calling the areAnagram method to verify if the anagram
 
         if(areAnagrams(arr[0], arr[1])){
             System.out.println("The given words are anagram");
@@ -41,20 +35,20 @@ public class Anagrams {
         }
     }
 
-    public static boolean areAnagrams(String word1, String word2){
+    public static boolean areAnagrams(String word1, String word2){  //to check the words if they are anagram
 
-        if(word1.length() != word2.length()){
+        if(word1.length() != word2.length()){     // execution breaks here if the inputs are invalid
             System.out.println("Words are not of same length");
             return false;
         }
 
-        char[] charArray1 = word1.toCharArray();
+        char[] charArray1 = word1.toCharArray();    //valid inputs are converted into character array
         char[] charArray2 = word1.toCharArray();
 
-        Arrays.sort(charArray1);
+        Arrays.sort(charArray1); // sorting each character arrays of the element
         Arrays.sort(charArray2);
 
-        return Arrays.equals(charArray1,charArray2);
+        return Arrays.equals(charArray1,charArray2); // returns if they are same
 
     }
 }
