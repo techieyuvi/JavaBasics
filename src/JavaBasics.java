af1c6250e4e3cf0a45b2eaa07796084f38d8a997
techieyuvi <yuvaraj.k1782@gmail.com>
2025-01-03 19:49:29 -0500

    Initial commit

---
+++ src/JavaBasics.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class JavaBasics{
+
+    public static void main(String[] args) {
+        String word;
+        String reversedWord ="";
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the word:");
+        word = sc.nextLine();
+
+        String[] arr = word.split(" ");
+
+        for (int i = arr.length - 1;i >= 0;i--) {
+            reversedWord +=  arr[i] + " ";
+        }
+        System.out.println(reversedWord);
+    }
+}
