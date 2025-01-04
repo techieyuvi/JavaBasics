8491c0b40b6b48f73f243ff3d30343412c173a02
techieyuvi <yuvaraj.k1782@gmail.com>
2025-01-05 02:53:32 +0530

    updating docs

--- src/ArmstrongNumber.java
+++ src/ArmstrongNumber.java
@@ -1,31 +1,29 @@
 import java.util.Scanner;
 
-
-
 public class ArmstrongNumber {
 
-    public static boolean tofindArmstrong(int number){
-        int originalNum = number;
-        int sum = 0;
-        int numDigits = String.valueOf(number).length();
+    public static boolean toFindArmstrong(int number){
+        int originalNum = number;    // saving the original number to compare
+        int sum = 0;                 // initializing the sum to zero
+        int numDigits = String.valueOf(number).length();     // getting the total number of digits in the input
 
-        while(number != 0){
+        while(number != 0){          // checking if its armstrong or not.
             int digit = number % 10;
             sum += Math.pow(digit,numDigits);
             number /= 10;
         }
-        return sum == originalNum;
+        return sum == originalNum;  // comparing the checked number with Original num.
     }
 
     public static void main(String[] args) {
         int num;
         Scanner input = new Scanner(System.in);
         num = input.nextInt();
-        input.close();
+        input.close();          //closing the scanner obj
 
-        tofindArmstrong(num);
+        toFindArmstrong(num);   //calling the method
 
-        if(tofindArmstrong(num)){
+        if(toFindArmstrong(num)){
             System.out.println("The given number is Armstrong number");
         }else {
             System.out.println("The given number is not a Armstrong number");
